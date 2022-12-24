4884923e8e80d200dafa7f691aa0a7ffe0a83cc8
m-arusu <106366676+m-arusu@users.noreply.github.com>
2022-12-24 00:18:52 -0500

    Upload q1-q3 in folder

---
+++ Easy/q3.sql
@@ -0,0 +1,5 @@
+# Tesla is investigating bottlenecks in their production, and they need your help to extract the relevant data. Write a SQL query that determines which parts have begun the assembly process but are not yet finished.
+
+SELECT DISTINCT part
+FROM parts_assembly
+WHERE finish_date IS NULL;
